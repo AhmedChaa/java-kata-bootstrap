aca80628fc53cacf1cb15f4d012ea1d01f32d3ea
AhmedChaa <chaari.ahmed89@gmail.com>
2020-10-05 16:03:26 +0200

    add PrimeFactors kata

--- src/main/java/com/kata/fun/PrimeFactors.java
+++ src/main/java/com/kata/fun/PrimeFactors.java
@@ -4,7 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PrimeFactors {
-    public static List<Integer> generate(int i) {
-        return new ArrayList<>();
+    public static List<Integer> generate(int n) {
+        List<Integer> primes = new ArrayList<>();
+        int candidate = 2;
+        while (n > 1) {
+            while (n % candidate == 0) {
+                primes.add(candidate);
+                n /= candidate;
+            }
+            candidate++;
+        }
+        return primes;
     }
 }
--- src/test/java/com/kata/fun/PirmeFactorsTest.java
+++ src/test/java/com/kata/fun/PirmeFactorsTest.java
@@ -7,15 +7,76 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.kata.fun.PrimeFactors.generate;
+
 @DisplayName("PrimeFactorsTest")
 public class PirmeFactorsTest {
 
-    private List<Integer> list() {
-        return new ArrayList<>();
+    private List<Integer> list(int... ints) {
+        List<Integer> list = new ArrayList<>();
+        for (int i : ints)
+            list.add(i);
+        return list;
     }
 
     @Test
     public void testOne() {
-        Assertions.assertEquals(list(),PrimeFactors.generate(1));
+        Assertions.assertEquals(list(), generate(1));
     }
+
+    @Test
+    public void testTwo() {
+        Assertions.assertEquals(list(2), generate(2));
+    }
+
+    @Test
+    public void testThree() {
+        Assertions.assertEquals(list(3), generate(3));
+    }
+
+    @Test
+    public void testFour() {
+        Assertions.assertEquals(list(2, 2), generate(4));
+    }
+
+    @Test
+    public void testFive() {
+        Assertions.assertEquals(list(5), generate(5));
+    }
+
+    @Test
+    public void testSix() {
+        Assertions.assertEquals(list(2, 3), generate(6));
+    }
+
+    @Test
+    public void testEight() {
+        Assertions.assertEquals(list(2, 2, 2), generate(8));
+    }
+
+    @Test
+    public void testSeven() {
+        Assertions.assertEquals(list(7), generate(7));
+    }
+
+    @Test
+    public void testNine() {
+        Assertions.assertEquals(list(3, 3), generate(9));
+    }
+
+    @Test
+    public void testTen() {
+        Assertions.assertEquals(list(2, 5), generate(10));
+    }
+
+    @Test
+    public void testTwenty() {
+        Assertions.assertEquals(list(2, 2, 5), generate(20));
+    }
+
+    @Test
+    public void testTwentyOne() {
+        Assertions.assertEquals(list(3, 7), generate(21));
+    }
+
 }
